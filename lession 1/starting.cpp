96a0fc52c709378b9006b11e03375471b8f9007f
xingguangcuican6666 <119158495+xingguangcuican6666@users.noreply.github.com>
2024-05-25 02:46:21 +0000

    first upload

---
+++ lession 1/starting.cpp
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int something; //int代表只支持整数数字 
+string nihao; //string可以存储任何东西
+double double1; //double可以存储数字后的小数点后64位
+int main()   //定义函数
+{
+    std::cin >> nihao; //表示让用户输入字符
+    return 0;
+}
